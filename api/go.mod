77bb2af4ebc3a1fe76deb3120b9ba4472b369b86
ezekiel <ezekiel@itsmart.my>
2024-07-16 21:37:52 +0800

    added files

---
+++ api/go.mod
@@ -0,0 +1,14 @@
+module github.com/ezekielchow/huma-test
+
+go 1.22.3
+
+require (
+	github.com/danielgtaylor/huma/v2 v2.19.0
+	github.com/go-chi/chi/v5 v5.0.12
+)
+
+require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/cobra v1.8.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+)
---
+++ api/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/danielgtaylor/huma/v2/adapters/humachi"
+	"github.com/danielgtaylor/huma/v2/humacli"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type Options struct {
+	Port int `help:"Port to listen on" short:"p" default:"8888"`
+}
+
+// GreetingOutput represents the greeting operation response.
+type GreetingOutput struct {
+	Body struct {
+		Message string `json:"message" example:"Hello, world!" doc:"Greeting message"`
+	}
+}
+
+type ReviewInput struct {
+	Body struct {
+		Author  string `json:"author" maxLength:"10" doc:"Author of the review"`
+		Rating  int    `json:"rating" minimum:"1" maximum:"5" doc:"Rating from 1 to 5"`
+		Message string `json:"message,omitempty" maxLength:"100" doc:"Review message"`
+	}
+}
+
+func addRoutes(api huma.API) {
+	// Register GET /greeting/{name}
+	huma.Register(api, huma.Operation{
+		OperationID: "get-greeting",
+		Method:      http.MethodGet,
+		Path:        "/greeting/{name}",
+		Summary:     "Get a greeting",
+		Description: "Get a greeting for a person by name.",
+		Tags:        []string{"Greetings"},
+	}, func(ctx context.Context, input *struct {
+		Name string `path:"name" maxLength:"30" example:"world" doc:"Name to greet"`
+	}) (*GreetingOutput, error) {
+		resp := &GreetingOutput{}
+		resp.Body.Message = fmt.Sprintf("Hello, %s!", input.Name)
+		return resp, nil
+	})
+
+	// Register POST /reviews
+	huma.Register(api, huma.Operation{
+		OperationID:   "post-review",
+		Method:        http.MethodPost,
+		Path:          "/reviews",
+		Summary:       "Post a review",
+		Tags:          []string{"Reviews"},
+		DefaultStatus: http.StatusCreated,
+	}, func(ctx context.Context, i *ReviewInput) (*struct{}, error) {
+		// TODO: save review in data store.
+		return nil, nil
+	})
+}
+
+func main() {
+
+	cli := humacli.New(func(hooks humacli.Hooks, options *Options) {
+
+		router := chi.NewMux()
+		router.Use(middleware.RequestID)
+		router.Use(middleware.RealIP)
+		router.Use(middleware.Logger)
+		router.Use(middleware.Recoverer)
+
+		router.Use(middleware.Timeout(60 * time.Second))
+
+		api := humachi.New(router, huma.DefaultConfig("my api", "1.0.0"))
+
+		addRoutes(api)
+
+		hooks.OnStart(func() {
+			fmt.Printf("Starting server on port %d...\n", options.Port)
+			http.ListenAndServe(fmt.Sprintf(":%d", options.Port), router)
+		})
+	})
+
+	cli.Run()
+}
